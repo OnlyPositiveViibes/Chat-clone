199b468ede7362f148bf93d6854d951ffff48e5a
Edvin <85315080+OnlyPositiveViibes@users.noreply.github.com>
2022-01-29 17:11:16 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {ChatEngine} from "react-chat-engine";
+import ChatFeed from './components/ChatFeed'
 
 import "./App.css";
 
@@ -9,9 +10,9 @@ const App = () => {
           projectID="73cb4501-d20a-4b4b-bf39-0569c883c371"
           userName="Edvin"
           userSecret="123123"
-        
+          renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/>}
         />
-    )
+    );
 }
 
 export default App;
